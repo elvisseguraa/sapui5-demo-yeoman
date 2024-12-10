bcd44c7a8dbed9f4b608107cc23d054f1d0c8d6e
Elvis Segura <elvisseguraa.5@outlook.com>
2024-12-10 15:41:29 -0500

    sapui5 with yeoman - basic app

--- myui5app/webapp/controller/ProductsView.controller.js
+++ myui5app/webapp/controller/ProductsView.controller.js
@@ -7,10 +7,18 @@ sap.ui.define([
     function (Controller) {
         "use strict";
 
-        return Controller.extend("myui5app.controller.MainView", {
+        return Controller.extend("myui5app.controller.ProductsView", {
             onInit: function () {
 
             },
+
+            handleListItemPress: function(oEvent) {
+				const oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+				const selectedProductId = oEvent.getSource().getBindingContext().getProperty("ProductID");
+				oRouter.navTo("RouteProductDetail", {
+					productId: selectedProductId
+				});
+			},
 
             onBeforeRendering: function() {
                 console.log("Antes de renderizar");
